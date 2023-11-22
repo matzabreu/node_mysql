a95deab6a54ed0ad131f57994056583cbc792df9
matzabreu <abreumagalhaes@outlook.com>
2023-11-21 22:35:56 -0300

    Update de Dados

--- index.js
+++ index.js
@@ -18,6 +18,24 @@ app.use(express.urlencoded({
 
 app.use(express.json());
 
+app.post("/edit/save", (request, response) => {
+    const { id, title, pageqty } = request.body
+
+    const sql = `
+        UPDATE books
+        SET title =  '${title}', pageqty = '${pageqty}'
+        WHERE id = ${id}
+    `
+
+    conn.query(sql, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        response.redirect("/")
+    })
+})
+
 app.post("/register/save", (request, response) => {
     const { title, pageqty } = request.body
     const query = `
@@ -32,6 +50,25 @@ app.post("/register/save", (request, response) => {
         }
 
         response.redirect("/")
+    })
+})
+
+app.get("/edit/:id", (request, response) => {
+    const id = request.params.id
+
+    const sql = `
+        SELECT * FROM books
+        WHERE id = ${id}
+    `
+
+    conn.query(sql, (error, data) => {
+        if(error) {
+            return console.log(error)
+        }
+
+        const book = data[0]
+
+        response.render('edit', { book })
     })
 })
 
